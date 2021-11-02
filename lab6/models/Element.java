5ffff7ad4b74813d8d33690461dd2c2f5d21a600
MihaiFironda <73839854+MihaiFironda@users.noreply.github.com>
2021-11-02 09:24:08 +0200

    .

---
+++ lab6/models/Element.java
@@ -0,0 +1,14 @@
+ge lab6.models;
+
+
+public interface Element {
+	//public void print();
+	
+	public void add(Element e) throws Exception;
+	
+	public Element get(int poz);
+	
+	public void remove(Element e); 
+
+	public boolean find(Element e);
+}
